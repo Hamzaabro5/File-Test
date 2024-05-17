83a06cca3814aaa01e5e3027562e4b18ae5bc96c
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-17 10:16:18 +0500

    File Solving

---
+++ app.js
@@ -0,0 +1,71 @@
+// // Answer No 1
+
+// let arr = [`a` , `e` , `i` , `o` , `u`]
+// const input = prompt(`Enter Your Input?`)
+
+// if (input.includes(arr)) {
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(arr[i]);
+        
+//     }
+// }
+
+// // Answer No 1
+
+
+
+
+
+
+
+
+
+
+
+// // Answer No 2
+
+// let arr =  [3, 7, 69, 12, 5, 9 , 86 , 6 , 5];
+
+// let input = prompt(`Write a number of array?`)
+
+// for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+//     // console.log(input);
+
+//     if (input.includes(arr)) {
+//         console.log(`${input} Apka dia hua number array of numbers m mojood h`);
+//     }else if (input >= arr) {
+//         console.log(`${input} Apka dia hua number array of numbers se barha h`);
+//     }else{
+//         console.log(`${input} Apka dia hua number array of numbers se Chota h`);
+//     }
+    
+// }
+
+// // Answer No 2
+
+
+
+
+
+
+
+
+
+
+// // // Answer No 4
+
+
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown'];
+
+// const container = document.getElementById('container'); // assuming you have a container element with id 'container'
+
+// for (let color of colors) {
+//     const div = document.createElement('div');
+//     div.style.backgroundColor = color;
+//     div.style.width = '100px';
+//     div.style.height = '100px';
+//     container.appendChild(div);
+// }
+
+// // // Answer No 4
